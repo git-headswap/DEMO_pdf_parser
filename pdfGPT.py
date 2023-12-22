e501ad13e2faf48c33fd8de6b08045e911596383
dberweger2017 <dberweger@gmail.com>
2023-12-22 22:22:51 +0100

    Refactor sidebar and add token calculator functionality

--- pdfGPT.py
+++ pdfGPT.py
@@ -64,85 +64,82 @@ def tik(words, model="cl100k_base"):
     tokens = encoding.encode(words)
     return tokens
 
-def main():
+def sidebar():
     with st.sidebar:
         API_KEY = st.text_input("Enter your API key")
         if API_KEY:
             if len(API_KEY) > 45 or len(API_KEY) < 40:
                 st.warning("Please enter a valid API key")
                 API_KEY = None
-        if not API_KEY:
-            # check if we are in light or dark mode
-
-            theme = st.get_option("theme.primaryColor")
-
-            st.image("static/instructions.png")
-
-            #if theme == "light":
-            #    st.image("static/instructions_light.jpg")
-            #else:
-            #    st.image("static/instructions_dark.jpg")
-            
-
-    with tab1:
-        st.header("PDF parsing demo")
-
-        if API_KEY:
-            info_to_extract = st.text_area("Info to extract", "Policy number, type of Policy, insurance, company, name of policy holder, start/end of Policy and birth date")
-
-            pdf_file = st.file_uploader("Upload a pdf")
-
-            if pdf_file:
-                text = ocrPDF(pdf_file)
-                autoGPT(text, info_to_extract, API_KEY)
-                st.write("To check the ongoing bill, you can click [here](https://api.headswap.com/)")
-
         else:
-            st.warning("Please enter your Headswap-API key in the sidebar to use this tab")
+            st.image("static/instructions.png")
+    return API_KEY
 
+def pdfParsingDemo(API_KEY):
+    st.header("PDF parsing demo")
+    if API_KEY:
+        info_to_extract = st.text_area("Info to extract", "Policy number, type of Policy, insurance, company, name of policy holder, start/end of Policy and birth date")
+
+        pdf_file = st.file_uploader("Upload a pdf")
+
+        if pdf_file:
+            text = ocrPDF(pdf_file)
+            autoGPT(text, info_to_extract, API_KEY)
+            st.write("To check the ongoing bill, you can click [here](https://api.headswap.com/)")
+
+    else:
+        st.warning("Please enter your Headswap-API key in the sidebar to use this tab")
+
+def tokenCalculator():
+    st.header("Token & Price calculator")
+    model = st.selectbox("Model", ["GPT-3", "GPT-4"])
+
+    prompt = st.text_area("Insert prompt text")
+
+    if prompt:
+        char_count = len(prompt)
+        num_tokens = tik(prompt)
+
+        GPT3_MAX_TOKEN_COUNT = 16_385
+        GPT4_MAX_TOKEN_COUNT = 128_000
+
+        if model == "GPT-3":
+            MAX_TOKEN_COUNT = GPT3_MAX_TOKEN_COUNT
+        elif model == "GPT-4":
+            MAX_TOKEN_COUNT = GPT4_MAX_TOKEN_COUNT
+        else:
+            st.write("Please select a model")
+
+        information = {
+            "Character count": char_count,
+            "Number of words": len(prompt.split()),
+            "Number of tokens": len(num_tokens),
+            "Percentage context length": f"{round(len(num_tokens) / MAX_TOKEN_COUNT * 100, 2)}%"
+        }
+
+        st.write(information)
+
+        if len(num_tokens) > MAX_TOKEN_COUNT:
+            st.warning(f"Your prompt exceeds the maximum token count of {MAX_TOKEN_COUNT} tokens for the selected model. Please reduce the number of tokens.")
+
+        st.header("Price ($USD)")
+
+        GPT3_PRICE = 0.001 / 1000
+        GPT4_PRICE = 0.01 / 1000
+
+        if model == "GPT-3":
+            st.write(f"Price: {round(len(num_tokens) * GPT3_PRICE, 4)}$")
+        elif model == "GPT-4":
+            st.write(f"Price: {round(len(num_tokens) * GPT4_PRICE, 4)}$")
+        else:
+            st.write("Please select a model")
+
+def main():
+    API_KEY = sidebar()
+    with tab1:
+        pdfParsingDemo(API_KEY)
     with tab2:
-        st.header("Token & Price calculator")
-        model = st.selectbox("Model", ["GPT-3", "GPT-4"])
-
-        prompt = st.text_area("Insert prompt text")
-
-        if prompt:
-            char_count = len(prompt)
-            num_tokens = tik(prompt)
-
-            GPT3_MAX_TOKEN_COUNT = 16_385
-            GPT4_MAX_TOKEN_COUNT = 128_000
-
-            if model == "GPT-3":
-                MAX_TOKEN_COUNT = GPT3_MAX_TOKEN_COUNT
-            elif model == "GPT-4":
-                MAX_TOKEN_COUNT = GPT4_MAX_TOKEN_COUNT
-            else:
-                st.write("Please select a model")
-
-            information = {
-                "Character count": char_count,
-                "Number of words": len(prompt.split()),
-                "Number of tokens": len(num_tokens),
-                "Percentage context length": f"{round(len(num_tokens) / MAX_TOKEN_COUNT * 100, 2)}%"
-            }
-
-            st.write(information)
-
-            if len(num_tokens) > MAX_TOKEN_COUNT:
-                st.warning(f"Your prompt exceeds the maximum token count of {MAX_TOKEN_COUNT} tokens for the selected model. Please reduce the number of tokens.")
-
-            st.header("Price ($USD)")
-
-            GPT3_PRICE = 0.001 / 1000
-            GPT4_PRICE = 0.01 / 1000
-
-            if model == "GPT-3":
-                st.write(f"Price: {round(len(num_tokens) * GPT3_PRICE, 4)}$")
-            elif model == "GPT-4":
-                st.write(f"Price: {round(len(num_tokens) * GPT4_PRICE, 4)}$")
-            else:
-                st.write("Please select a model")
-
+        tokenCalculator()
+        
 if __name__ == "__main__":
     main()
