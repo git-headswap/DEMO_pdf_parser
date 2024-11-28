0a52835f84450d60c14a68cf498d701a7b7755ad
KhilchandHS <khilchand.patil@headswap.com>
2024-11-28 15:15:36 +0530

    revert v3

--- pdfGPT.py
+++ pdfGPT.py
@@ -164,7 +164,7 @@ def lightningOutFlowDemo():
             }
 
             // Retrieve the recordId from the URL
-            const recordId = getQueryParam('recordId') || '001FS00000iSZG3YAO'; // Default value if not provided
+            const recordId = getQueryParam('recordId'); //|| '001FS00000iSZG3YAO'; // Default value if not provided
             //const paramValue = getQueryParam(recordId);   
                 if (recordId) {
                     alert(`The value of the "${recordId}" `);
